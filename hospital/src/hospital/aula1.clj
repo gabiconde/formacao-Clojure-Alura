c78b1b39e7f1205d5d0f0004ee85648e03cae3d5
Gabriela Conde <gabriela.conde@nubank.com.br>
2019-12-04 12:22:16 -0300

    utilizando threads paralelas

--- hospital/src/hospital/aula1.clj
+++ hospital/src/hospital/aula1.clj
@@ -15,3 +15,23 @@
   (pprint hospital))
 
 ;(simula-um-dia)
+
+(defn chega-em-malvado [pessoa]
+  (def hospital (h.logic/checkin-pausado hospital :espera pessoa))
+  (println "apos inserir" pessoa))
+
+
+(defn simula-um-dia-em-paralelo
+  []
+  (def hospital (h.model/novo-hospital))
+  (.start (Thread. (fn [] (chega-em-malvado "111"))))
+  (.start (Thread. (fn [] (chega-em-malvado "222"))))
+  (.start (Thread. (fn [] (chega-em-malvado "333"))))
+  (.start (Thread. (fn [] (chega-em-malvado "444"))))
+  (.start (Thread. (fn [] (chega-em-malvado "555"))))
+  (.start (Thread. (fn [] (chega-em-malvado "666"))))
+
+  (.start (Thread. (fn [] (Thread/sleep 4000)
+                     (pprint hospital)))))
+
+(simula-um-dia-em-paralelo)
--- hospital/src/hospital/logic.clj
+++ hospital/src/hospital/logic.clj
@@ -15,3 +15,10 @@
 (defn atende
   [hospital departamento]
   (update hospital departamento pop))
+
+(defn checkin-pausado
+  [hospital departamento pessoa]
+  (if (cabe-na-fila? hospital departamento)
+    (do (Thread/sleep 1000)
+        (update hospital departamento conj pessoa))
+    (throw (ex-info "Fila já está cheia" {:tentando-adicionar pessoa}))))
